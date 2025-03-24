8ff063de82e800d4c22d8716ff41a42f6c415d79
anyaelissa <schwanesa@sfusd.edu>
2025-03-24 08:45:12 -0700

    Initial Evolvinator commit
    Initial commit of evolvinator files

---
+++ Evolvinator/Evo_OD.ino
@@ -0,0 +1,116 @@
+/* 2 OD --------------------------------------------------------------------
+ 2a ODRead
+ 2b ODCalibrate
+ */
+
+/* >>>>>>>>>>>>>>>>>>>>> Constants and Variables <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
+
+float diodeReading;                         // raw diode reading [0-1023]                      
+float ODZeroPrint = 0;                    // convert to 1 cm 
+int ODCount = 0;
+
+/* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Functions <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
+// 2 ODRead 
+float ODRead() {
+  // Local Variables
+  int photoRead;                                  // photodetector reading
+  float diodeLightOnAvg = 0;
+  float diodeLightOffAvg = 0;
+  static unsigned long oldMsOD10MinAvg = 0;       // static variables are only declared the first time the function runs
+
+  digitalWrite(pinValve, HIGH);                   // close valve
+  delay(2000);                                    // delay to let bubbles dissapate
+
+  for (int x = 9; x > 0; x--) {                   // move data down for 10 min avg
+    ODMin[x] = ODMin[x - 1];
+  }
+
+  // Determine OD
+  diodeReading = 0;
+  for (int y = 0; y < 5; y++) {                   // average over 5 readings
+    photoRead = 0;
+    for (int x = 0; x < 30; x++) {                // consisiting of 30 readings each
+      photoRead += analogRead(pinODRead);         // read data
+      delay(10);
+    }
+    diodeLightOffAvg = photoRead / 30.0;            // average
+    digitalWrite(pinODLED, HIGH);                 // turn light on
+    delayMicroseconds(1);                         // give the photodiode a microsecond to adjust
+    photoRead = 0;
+    for (int x = 0; x < 30; x++) {
+      photoRead += analogRead(pinODRead);
+      delay(10);
+    }
+    diodeLightOnAvg = photoRead / 30.0;
+    diodeReading += diodeLightOnAvg - diodeLightOffAvg;
+    digitalWrite(pinODLED, LOW);                  // turn light off
+    delayMicroseconds(1);
+  }
+
+  digitalWrite(pinValve, LOW);                    // turn air back on
+
+  diodeReading /= 5;
+  if (diodeReading > 1) {                         // correct for super high OD
+    diodeReading = 1;
+  }
+  ODMin[0] = float(diodeReading);
+  ODMin[0] = (log(1 / (ODMin[0] / ODZero)) / log(10));  //gives NaN until calibrated
+  ODMin[0] = ODMin[0] * .9861 + 0.0058;           // **TRANSFER FUNCTION FROM CALIBRATION**
+  if (ODMin[0] < 0) {                             // set it to 0 if negative value is read
+    ODMin[0] = 0;
+  }
+  OD3MinAvg = (ODMin[0] + ODMin[1] + ODMin[2]) / 3;
+
+  ODCount += 1; 
+  SDDataLog('o', ODCount, 0);                     // save data point to SD card
+
+  //if (debugMode) Serial.println(ODMin[0]);
+
+  return ODMin[0], OD3MinAvg, diodeReading;
+}
+
+// 2b ODCalibrate
+float ODCalibrate() {
+  int photoRead;                                 
+  int diodeLightOnAvg = 0;
+  int diodeLightOffAvg = 0;
+
+  digitalWrite(pinValve, HIGH);                     
+  delay(2000);                                     
+
+  ODZero = 0;                                       
+  for (int y = 0; y < 10; y++) {
+    photoRead = 0; 
+    for (int x = 0; x < 30; x++) {             //take 30 readings
+      photoRead += analogRead(pinODRead);
+      delay(10);
+    }
+    diodeLightOffAvg = photoRead / 30;        // average readings with LED off
+    digitalWrite(pinODLED, HIGH);             // turns LED on (HIGH)       
+    delayMicroseconds(1);                            
+    photoRead = 0;
+    for (int x = 0; x < 30; x++) {            //take 30 readings
+      photoRead += analogRead(pinODRead);
+      delay(10);
+    }
+    diodeLightOnAvg = photoRead / 30;         // average readings with LED on
+    ODZero += float(diodeLightOnAvg - diodeLightOffAvg);     // ODZero = light on - light off 
+    digitalWrite(pinODLED, LOW);                // turns LED off
+    delayMicroseconds(1);
+  }
+  ODZero /= 10;                                 // Avg ODZero readings
+  
+
+  if (tStart) {
+    digitalWrite(pinValve, LOW);
+  }
+
+  if (debugMode) {
+    Serial.print("Averaged ODZero is ");
+    Serial.println(ODZero);
+  }
+  return ODZero;
+}
+
+
+
---
+++ Evolvinator/arduino_secrets_example.h
@@ -0,0 +1,8 @@
+// Copy this file to "arduino_secrets.h" and add your own secrets.
+//
+// The arduino_secrets.h file is not tracked in git because we don't want to check
+// our secrets into a repo that may become public at some point in the future.
+
+
+#define SECRET_ADAFRUIT_IO_USERNAME  "schwanesa"
+#define SECRET_ADAFRUIT_IO_KEY       "aio_McYw02L5Yh7TKkwdcFNdhfL6KSnc"
